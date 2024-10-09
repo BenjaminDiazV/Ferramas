c2d1d15d43830bc0a96cbe29b45d1ceb9b2ec1ba
Benjamín Díaz <benj.diazv@duocuc.cl>
2024-10-09 18:20:35 -0300

    1.0
    Ahora con uso de credencial.txt y con la serie especifica

--- main/services.py
+++ main/services.py
@@ -1,43 +1,51 @@
 import bcchapi;
 import requests;
+from datetime import date;
 
-# def usarSerie():
+def obtener_credenciales(ruta_archivo):
+    with open(ruta_archivo, 'r') as file:
+        user = file.readline().strip()  # Leer la primera línea (usuario)
+        password = file.readline().strip()  # Leer la segunda línea (contraseña)
+    return user, password
 
-#     user="marjuan9876@gmail.com"
+def usarSerie():
+
+
+    user, password = obtener_credenciales('C:/Users/benja/OneDrive/Escritorio/Ferramas/ferramas/main/credenciales.txt')
+
+    fecha1=str(date.today())
+    fecha2=str(date.today())
+
+    apiUrl = f"https://si3.bcentral.cl/SieteRestWS/SieteRestWS.ashx?user={user}&pass={password}&firstdate={fecha1}&lastdate={fecha2}&timeseries=F073.TCO.PRE.Z.D&function=GetSeries"
+    respuesta = requests.get(apiUrl)
+
+    if respuesta.status_code == 200:
+        data = respuesta.json()
+        valor = data['Series']['Obs'][0]['value']
+        return "valor de cambio del dolar: "+ valor
+    else:
+        return f"Error {respuesta.status_code}: {respuesta.text}"
+
+resultado = usarSerie()
+print(resultado)
+
+
+# def buscarSeries():     
+#     user="marjuan9876@gmail.com"     
 #     password="Marjuan_12"
 
-#     fecha1="2021-10-10"
-#     fecha2="2021-10-15"
-
-#     apiUrl = f"https://si3.bcentral.cl/SieteRestWS/SieteRestWS.ashx?user={user}&pass={password}&firstdate={fecha1}&lastdate={fecha2}&timeseries=F022.TPM.TIN.D001.NO.Z.D&function=GetSeries"
+#     apiUrl = f"https://si3.bcentral.cl/SieteRestWS/SieteRestWS.ashx?user={user}&pass={password}&function=SearchSeries&frequency=ANNUAL"
 #     respuesta = requests.get(apiUrl)
 
 #     if respuesta.status_code == 200:
 #         data = respuesta.json()
 #         return data
 #     else:
-#        return f"Error {respuesta.status_code}: {respuesta.text}"
-
-# resultado = usarSerie()
-# print(resultado)
-
-
-def buscarSeries():     
-    user="marjuan9876@gmail.com"     
-    password="Marjuan_12"
-
-    apiUrl = f"https://si3.bcentral.cl/SieteRestWS/SieteRestWS.ashx?user={user}&pass={password}&function=SearchSeries&frequency=ANNUAL"
-    respuesta = requests.get(apiUrl)
-
-    if respuesta.status_code == 200:
-        data = respuesta.json()
-        return data
-    else:
-        return f"Error {respuesta.status_code}: {respuesta.text}"
+#         return f"Error {respuesta.status_code}: {respuesta.text}"
 
         
-resultado = buscarSeries()
-print(resultado)
+# resultado = buscarSeries()
+# print(resultado)
     
 
 
