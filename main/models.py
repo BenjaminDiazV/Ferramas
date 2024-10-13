20d8a67cf0dac300b8179402408e301ccffa704b
Benjamín Díaz <benj.diazv@duocuc.cl>
2024-10-13 20:44:25 -0300

    1.1
    arreglado el css y agregado el modelo y super usuario

--- main/models.py
+++ main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    id_categoria = models.AutoField(db_column='idCategoria',primary_key=True)
+    nombre_c = models.CharField(max_length=50,blank=False,null=False)
+
+    def __str__(self):
+        return str(self.nombre_c)
+    
+class Producto(models.Model):
+    id_producto = models.AutoField(primary_key=True)
+    nom_producto = models.CharField(max_length=200,blank=False,null=False)
+    categoria = models.ForeignKey('Categoria', on_delete=models.CASCADE, db_column='idCategoria')
+    marca = models.CharField(max_length=100,null=False)
+    valor = models.IntegerField()
