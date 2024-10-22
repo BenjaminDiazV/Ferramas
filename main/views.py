f1a4bf464f2024fa040662a99114e5d6acf6aaf4
Benjamín Díaz <benj.diazv@duocuc.cl>
2024-10-22 13:07:19 -0300

    1.4
    avance

--- main/views.py
+++ main/views.py
@@ -4,10 +4,16 @@ from .models import Producto
 # Create your views here.
 
 def index(request):
+    context={}
+    
+    return render(request, 'pages/index.html', context)
+
+def catalogo(request):
     producto = None
     if request.method == "POST":
         producto_id = request.POST.get('id_producto')
         if producto_id:
              producto = Producto.objects.get(id_producto=producto_id)
     
-    return render(request, 'main/index.html', {'producto':producto})
+
+    return render(request, 'pages/catalogo.html', {'producto':producto})
